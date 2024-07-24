15a07f70dd343f385341499552a1f5f1f81d1bd1
Peter Jones <pete@peterellisjones.com>
2024-07-24 08:34:47 +0100

    Update settings language

--- SmartOrders/SmartOrdersPlugin.cs
+++ SmartOrders/SmartOrdersPlugin.cs
@@ -7,7 +7,8 @@ using Serilog;
 using UI.Builder;
 
 [UsedImplicitly]
-public sealed class SmartOrdersPlugin : SingletonPluginBase<SmartOrdersPlugin>, IModTabHandler {
+public sealed class SmartOrdersPlugin : SingletonPluginBase<SmartOrdersPlugin>, IModTabHandler
+{
 
     public static IModdingContext Context { get; private set; } = null!;
     public static IUIHelper UiHelper { get; private set; } = null!;
@@ -15,31 +16,36 @@ public sealed class SmartOrdersPlugin : SingletonPluginBase<SmartOrdersPlugin>, 
 
     private readonly ILogger _Logger = Log.ForContext<SmartOrdersPlugin>()!;
 
-    public SmartOrdersPlugin(IModdingContext context, IUIHelper uiHelper) {
+    public SmartOrdersPlugin(IModdingContext context, IUIHelper uiHelper)
+    {
         Context = context;
         UiHelper = uiHelper;
 
         Settings = Context.LoadSettingsData<Settings>("SmartOrders") ?? new Settings();
     }
 
-    public override void OnEnable() {
+    public override void OnEnable()
+    {
         _Logger.Information("OnEnable");
         var harmony = new Harmony("SmartOrders");
         harmony.PatchAll();
     }
 
-    public override void OnDisable() {
+    public override void OnDisable()
+    {
         _Logger.Information("OnDisable");
         var harmony = new Harmony("SmartOrders");
         harmony.UnpatchAll();
     }
 
-    public void ModTabDidOpen(UIPanelBuilder builder) {
-        builder.AddField("Debug", builder.AddToggle(() => Settings.EnableDebug, o => Settings.EnableDebug = o)!);
-        builder.AddField("Report in car lengths", builder.AddToggle(() => Settings.UseCarLengthInsteadOfFeet, o => Settings.UseCarLengthInsteadOfFeet = o)!);
+    public void ModTabDidOpen(UIPanelBuilder builder)
+    {
+        builder.AddField("Use in car lengths, not feet", builder.AddToggle(() => Settings.UseCarLengthInsteadOfFeet, o => Settings.UseCarLengthInsteadOfFeet = o)!);
+        builder.AddField("Send debug logs to console", builder.AddToggle(() => Settings.EnableDebug, o => Settings.EnableDebug = o)!);
     }
 
-    public void ModTabDidClose() {
+    public void ModTabDidClose()
+    {
         Context.SaveSettingsData("SmartOrders", Settings);
     }
 
